210bb395c585fd01fbe8188696e3c69579578d06
leedoyun98 <leedoyun98@naver.com>
2020-12-03 10:44:11 +0900

    1

---
+++ xmas-master/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,12 @@
+package com.example.demo.services;
+
+import com.example.demo.domains.UserDto;
+
+import org.springframework.stereotype.Component;
+
+
+public interface UserService {
+    public int join(UserDto user);
+
+	public UserDto login(UserDto user);
+}
